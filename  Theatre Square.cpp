cde5b94177e77a223151a163d84f810d11f38053
Haneen Ibrahim <91877743+HaneenIbrahim2@users.noreply.github.com>
2022-08-31 17:06:00 +0200

    Theatre Square

---
+++  Theatre Square.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+
+    long long n , m , a , x,y ;
+    cin>>n>>m>>a;
+    x=m/a;
+    y=n/a;
+    if(m%a!=0){
+        x++;
+    }if(n%a!=0){
+        y++;
+
+    }
+    cout<<x*y;
+
+}
