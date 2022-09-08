f8fcf7a0a010f6004790fd266375122218f39729
Haneen Ibrahim <91877743+HaneenIbrahim2@users.noreply.github.com>
2022-09-08 18:04:47 +0200

    Digits Summation

---
+++ Digits Summation.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    long  long n, m ,digit1,digit2  ,sum;
+    cin >> n >> m;
+    digit1 = n % 10;
+    digit2 = m % 10;
+    sum = digit1 + digit2;
+    cout << sum;
+
+
+
+
+
+}
+
