61c0619133f06d3f29d9e5cfffc919380fcf4d08
Haneen Ibrahim <91877743+HaneenIbrahim2@users.noreply.github.com>
2022-09-08 18:02:45 +0200

    Difference

---
+++ Difference.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    long long   a, b, c, d ,Differ  ;
+    cin >> a >> b >> c >> d;
+    Differ = (a * b) - (c * d);
+    cout << "Difference = " << Differ;
+
+
+}
