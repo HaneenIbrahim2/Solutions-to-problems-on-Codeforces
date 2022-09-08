a276bcbb0e315acf15be98d821838c83b198be2d
Haneen Ibrahim <91877743+HaneenIbrahim2@users.noreply.github.com>
2022-09-08 18:03:48 +0200

    Area of a Circle

---
+++ Area of a Circle.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+
+
+    cout << fixed <<setprecision( 9)  ;
+    const long double pi = 3.141592653;
+    long double R  ;
+
+    cin >> R;
+    cout << (   pi * (R* R) );
+
+
+
+
+}
