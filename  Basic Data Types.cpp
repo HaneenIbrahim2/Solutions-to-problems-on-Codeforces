39808bcba8c06c168b2dd4502ca70285b07cd2e5
Haneen Ibrahim <91877743+HaneenIbrahim2@users.noreply.github.com>
2022-09-08 18:00:55 +0200

    Basic Data Types

---
+++  Basic Data Types.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using  namespace std;
+int main()
+{
+    int n;
+    long long s;
+    char a;
+    float m;
+    double f;
+    cin >> n >> s >> a >> m >> f;
+    cout << n << endl;
+    cout << s << endl;
+    cout << a << endl;
+    cout << m << endl;
+    cout << f;
+
+}
