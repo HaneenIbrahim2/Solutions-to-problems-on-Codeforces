e5567bdf9d1d47bceb16771e98c5dd3d745cdfc6
Haneen Ibrahim <91877743+HaneenIbrahim2@users.noreply.github.com>
2022-09-08 16:35:32 +0200

    Magnets

---
+++ Magnets.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    bool  a ,b ,c, d, e ,f;
+    cin  >> a >>b>>c>>d>> e>>f;
+    if( (b!=c)&&(d!=e)){
+
+
+        cout<< "YES";
+    }else
+        cout<<"NO";
+
+
+
+    return 0;
+}
