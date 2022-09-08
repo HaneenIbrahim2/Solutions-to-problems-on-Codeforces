257d1c4c27421d030dea028b6b53f1d2859a7806
Haneen Ibrahim <91877743+HaneenIbrahim2@users.noreply.github.com>
2022-09-08 17:50:42 +0200

    Square

---
+++ Square.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    int n1 ,n2 ,n3 ,n4;
+    cin>>n1>>n2>>n3>>n4;
+    if(n1==n2&&n2==n3&&n3==n4){
+        cout<<"yes";
+    }else
+        cout<<"no";
+}
