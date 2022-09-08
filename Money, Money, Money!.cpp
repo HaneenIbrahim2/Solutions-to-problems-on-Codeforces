af5f2ed963af8ac484888e2f188daf5f1008b004
Haneen Ibrahim <91877743+HaneenIbrahim2@users.noreply.github.com>
2022-09-08 15:55:04 +0200

    Money, Money, Money!

---
+++ Money, Money, Money!.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long n,x,y;
+
+    cin >> n >> x >> y;
+
+    long long sum =n*y - n*x ;
+
+    cout  <<  sum;
+    return 0;
+}
