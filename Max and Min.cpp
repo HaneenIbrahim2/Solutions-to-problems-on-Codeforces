632f39d11413777160a1c384784991244d3b1b52
Haneen Ibrahim <91877743+HaneenIbrahim2@users.noreply.github.com>
2022-09-08 18:18:58 +0200

    Max and Min

---
+++ Max and Min.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    long long  a, b, c;
+    cin >> a >> b >> c;
+    cout << min(c, min(a, b)) << " " << max(c, max(a, b));
+
+
+
+
+
+
+
+}
