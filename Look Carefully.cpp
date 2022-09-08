f1a41dc88e7758a8613ace30d12d5ab5dd5448b6
Haneen Ibrahim <91877743+HaneenIbrahim2@users.noreply.github.com>
2022-09-08 17:26:32 +0200

    Look Carefully

---
+++ Look Carefully.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<iomanip>
+#include<cstdlib>
+#include<string>
+#include<cmath>
+#include <vector>
+using namespace std;
+
+int main()
+{
+
+    char a ;
+    cin >> a;
+    if (   a == 'C' || a == 'h' ||   a == 'i'  ) {
+        cout << "Yes";
+    }
+    else
+        cout << "No";
+}
