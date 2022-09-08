e8444d1ba8e7bce8cd9ca18c720eb89c189734bf
Haneen Ibrahim <91877743+HaneenIbrahim2@users.noreply.github.com>
2022-09-08 17:00:06 +0200

    Again Twenty Five!

---
+++ Again Twenty Five.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<iomanip>
+#include<cstdlib>
+#include<string>
+#include<cmath>
+#include <vector>
+using namespace std;
+int main()
+
+{
+    long long  n;
+    cin>>n;
+    if(n>2){
+        n=2;
+    }cout<<pow(5,n);
+
+
+
+
+
+
+
+
+
+}
