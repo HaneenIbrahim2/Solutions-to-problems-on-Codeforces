676715196334279c0e9cda669776e0a12b351abe
Haneen Ibrahim <91877743+HaneenIbrahim2@users.noreply.github.com>
2022-09-09 12:52:11 +0200

    Next Alphabet

---
+++  Next Alphabet,.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<iomanip>
+#include<cstdlib>
+#include<string>
+using namespace std;
+int main()
+{
+    char a;
+    cin >> a;
+
+    if (a == 'z') {
+        cout << "a";
+    }
+    else if (a != ' z') {
+        a++;
+        cout << a;
+    }
+
+
+
+}
