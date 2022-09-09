9905349f7f686faf520497d1e7b949c492f2145f
Haneen Ibrahim <91877743+HaneenIbrahim2@users.noreply.github.com>
2022-09-09 13:07:12 +0200

    Even Numbers

---
+++ Even Numbers.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<iomanip>
+#include<cstdlib>
+#include<string>
+#include<cmath>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        if (i % 2 == 0) {
+            cout << i << endl;
+
+        }
+
+
+    }
+    if (n == 1) {
+        cout << "-1";
+    }
+}
